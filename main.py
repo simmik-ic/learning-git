6f9cbb0f6be6c023416a855386b8af968e543acd
simmik-ic <simmik@poczta.onet.pl>
2025-02-28 14:43:46 +0100

    Update main.py - add fibonacci

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 for i in range(0,5):
     print("Uranus")
 print("A znasz może inne planety układu słonecznego?")
+
+num = 30
+fibonacci = [1]
+
+while len(fibonacci)<num:
+    fibonacci.append(sum(fibonacci[-2:]))
+print(fibonacci)
