042872b01f52b30a938320a8c459cc8fe49a9e57
Szymon Mikulski <simmik@poczta.onet.pl>
2025-02-28 14:58:12 +0100

    Update main.py - add greetings

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ for i in range(0,5):
     print("Uranus")
 print("A znasz może inne planety układu słonecznego?")
 
-num = 100
+num = 10
 fibonacci = [1]
 
 while len(fibonacci)<num:
     fibonacci.append(sum(fibonacci[-2:]))
 print(fibonacci)
+
+print("Ile to jeszcze będzie trwać?")
